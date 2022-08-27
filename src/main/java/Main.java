56c9b6f136ec8c22a01e5903428a8c39c258ff3c
kelvin <kelvin.ojeda.2020b@gmail.com>
2022-08-26 22:59:19 -0500

    Cambio del valor de la variable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-	System.out.println("Hello Word");
-	int a = 10;
-
+		System.out.println("Hello Word");
+		int a = 10;
+		int b = 10;
 	}
 
 }
